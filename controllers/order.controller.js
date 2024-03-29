e23544935d03a688469a1d6d86cf251131aafde3
nntruyen027 <nntruyen027@gmail.com>
2024-03-29 23:57:58 +0700

    sửa gio

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -3,11 +3,11 @@ const validate = require('../validations/order');
 
 exports.getAllOrders = async (req, res) => {
   try {
-    const orders = await Order.find({user: req.user._id}).populate('user').populate('items.product');
-    return res.status(200).send({data: orders});
+    const orders = await Order.find({ user: req.user._id }).populate('user').populate('items.product');
+    return res.status(200).send({ data: orders });
   } catch (error) {
     console.error(error.message);
-    res.status(500).send({error: 'Lỗi nội bộ'});
+    res.status(500).send({ error: 'Lỗi nội bộ' });
   }
 };
 
@@ -15,22 +15,23 @@ exports.getOrderById = async (req, res) => {
   try {
     const order = await Order.findById(req.params.id).populate('user').populate('items.product');
     if (!order) return res.status(404).json({ error: 'Không tìm thấy' });
-    return res.json({data: order});
+    return res.json({ data: order });
   } catch (error) {
     console.error(error.message);
-    res.status(500).send({error: 'Lỗi nội bộ'});
+    res.status(500).send({ error: 'Lỗi nội bộ' });
   }
 };
 
 exports.createOrder = async (req, res) => {
   const orderData = req.body;
 
-  const {error} = validate(orderData);
+  const { error } = validate(orderData);
 
-  if(error)
-    return res.status(400).send({error: error})
+  if (error) {
+    return res.status(400).send({ error: error });
+  }
 
-  const {paymentMethod, deliveryMethod, address, items} = orderData;
+  const { paymentMethod, deliveryMethod, address, items } = orderData;
   const user = req.user._id;
 
   try {
@@ -39,12 +40,14 @@ exports.createOrder = async (req, res) => {
     });
 
     await newOrder.save();
-    await newOrder.populate('user').populate('items.product').execPopulate();
 
-    return res.status(201).json({data: newOrder});
+    // Lấy thông tin của người dùng và sản phẩm sau khi đơn hàng đã được lưu
+    const populatedOrder = await Order.findById(newOrder._id).populate('user').populate('items.product');
+
+    return res.status(201).json({ data: populatedOrder });
   } catch (error) {
     console.log(error);
-    res.status(500).send({error: 'Lỗi nội bộ'});
+    res.status(500).send({ error: 'Lỗi nội bộ' });
   }
 };
 
@@ -58,7 +61,7 @@ exports.updateOrderById = async (req, res) => {
     if (!updatedOrder) return res.status(404).json({ error: 'Không tìm thấy' });
     res.json(updatedOrder);
   } catch (error) {
-    res.status(500).send({error: 'Lỗi nội bộ'});
+    res.status(500).send({ error: 'Lỗi nội bộ' });
   }
 };
 
@@ -71,6 +74,6 @@ exports.deleteOrderById = async (req, res) => {
     return res.sendStatus(204);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send({error: 'Lỗi nội bộ'});
+    res.status(500).send({ error: 'Lỗi nội bộ' });
   }
 };
