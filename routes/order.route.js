33d6f5003b00c544de0663a2c4b52cfcf8071e1d
nntruyen027 <nntruyen027@gmail.com>
2024-03-30 00:22:19 +0700

    sửa gio

--- routes/order.route.js
+++ routes/order.route.js
@@ -4,12 +4,14 @@ const controller = require('../controllers/order.controller');
 const { authenticateToken, isAdmin } = require('../middlewares/authentication');
 
 router.get('/', authenticateToken, controller.getAllOrders);
+router.get('/admin', authenticateToken, isAdmin, controller.getAllOrdersByAdmin);
+
 router.get('/:id', authenticateToken, controller.getOrderById);
 
 router.post('/', authenticateToken, controller.createOrder);
 
 router.delete('/:id', authenticateToken, controller.deleteOrderById);
 
-router.get('/admin', authenticateToken, isAdmin, controller.getAllOrdersByAdmin);
+
 
 module.exports = router;
