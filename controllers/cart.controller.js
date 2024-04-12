145e2d7e4aa0f3c320ec14180c13429aa3322822
nntruyen027 <nntruyen027@gmail.com>
2024-04-12 23:04:47 +0700

    Chỉnh sửa về status

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -40,7 +40,7 @@ exports.updateCartItem = async (req, res) => {
     if (itemIndex === -1) {
       return res.status(404).json({ error: 'Không tìm thấy sản phẩm' });
     }
-    
+
     const updatedItem = req.body;
     cart.items[itemIndex].quantity = updatedItem.quantity;
 
@@ -48,7 +48,7 @@ exports.updateCartItem = async (req, res) => {
     await cart.save();
     res.status(200).json(cart);
   } catch (error) {
-    print(error)
+    console.log(error)
     res.status(500).json({ error: 'Lỗi nội bộ' });
   }
 };
