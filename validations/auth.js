b60e2066da4d919cee263840c14944fa1dc1b4df
nntruyen027 <nntruyen027@gmail.com>
2024-04-20 02:36:52 +0700

    sửa tiếp

--- validations/auth.js
+++ validations/auth.js
@@ -1,16 +1,17 @@
 const Joi = require('joi');
 
 const registerValidator = (data) => {
+  const eighteenYearsAgo = new Date();
+  eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+
   const rule = Joi.object({
     username: Joi.string().min(5).max(255).required(),
     password: Joi.string().min(8).max(255).required(),
     fullname: Joi.string().min(6).max(255).required(),
-    birthday: Joi.date().max('now').required()
-      .less('18 years')
+    birthday: Joi.date().max(eighteenYearsAgo).required()
       .messages({
         'date.base': 'Ngày sinh phải là một ngày hợp lệ.',
-        'date.max': 'Ngày sinh không thể ở tương lai.',
-        'date.less': 'Bạn phải ít nhất 18 tuổi để đăng ký.'
+        'date.max': 'Bạn phải ít nhất 18 tuổi để đăng ký.'
       }),
     address: Joi.string(),
     email: Joi.string().email(),
